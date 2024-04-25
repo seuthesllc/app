8e73e2bd91efb6e798fd132a12d1f45c27174566
Bailey Pumfleet <bailey@pumfleet.co.uk>
2024-04-25 10:51:18 -0400

    Tailwind, Prisma, shadcn/ui

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,8 +1,9 @@
+import { Button } from "@/components/ui/button";
 import type { MetaFunction } from "@remix-run/node";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
+    { title: "Seuthes" },
     { name: "description", content: "Welcome to Remix!" },
   ];
 };
@@ -11,6 +12,7 @@ export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
       <h1>Welcome to Remix</h1>
+      <Button>Click me</Button>
       <ul>
         <li>
           <a
